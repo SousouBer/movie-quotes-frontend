a54c9b9aaf7b7f2ac4604e28b5288ba3259369ae
Soso <soso.beriashvili@redberry.ge>
2024-05-06 23:54:40 +0400

    refactor: separate base button and select input into components

--- src/components/shared/TheHeader.vue
+++ src/components/shared/TheHeader.vue
@@ -2,7 +2,8 @@
 import { ref } from 'vue'
 
 import IconNotification from '@/components/icons/IconNotification.vue'
-import IconDropdownLocale from '@/components/icons/IconDropdownLocale.vue'
+import BaseButton from '@/components/ui/BaseButton.vue'
+import InputSelect from '@/components/ui/InputSelect.vue'
 
 // Will change this with the real data.
 const isAuthenticated = ref<boolean>(false)
@@ -13,23 +14,9 @@ const isAuthenticated = ref<boolean>(false)
     <span class="uppercase text-base text-shade-of-beige font-medium">movie quotes</span>
     <div class="flex items-center gap-4">
       <IconNotification v-if="isAuthenticated" />
-      <div class="flex items-center gap-2 relative w-14 mr-6">
-        <select
-          class="cursor-pointer appearance-none bg-transparent outline-none w-full text-white text-base"
-          name="locales"
-          id="set-locale"
-        >
-          <option class="bg-black border-none text-xs p-0 hover:bg-red-500" value="en">Eng</option>
-          <option class="bg-black border-none text-xs" value="ka">Geo</option>
-        </select>
-        <IconDropdownLocale
-          class="pointer-events-none absolute top-0 right-0 transform translate-y-2.5 -translate-x-1/2"
-        />
-      </div>
-      <button class="text-white bg-vivid-red px-6 py-2 rounded-md">Sign up</button>
-      <button class="text-white bg-transparent px-6 py-2 rounded-md border border-white">
-        Sign up
-      </button>
+      <InputSelect />
+      <BaseButton label="Sign up" />
+      <BaseButton label="Log in" :hasBorder="true" />
     </div>
   </nav>
 </template>
---
+++ src/components/ui/InputSelect.vue
@@ -0,0 +1,19 @@
+<script setup lang="ts">
+import IconDropdownLocale from '@/components/icons/IconDropdownLocale.vue'
+</script>
+
+<template>
+  <div class="flex items-center gap-2 relative w-14 mr-6">
+    <select
+      class="cursor-pointer appearance-none bg-transparent outline-none w-full text-white text-base"
+      name="locales"
+      id="set-locale"
+    >
+      <option class="bg-black border-none text-xs" value="en">Eng</option>
+      <option class="bg-black border-none text-xs" value="ka">Geo</option>
+    </select>
+    <IconDropdownLocale
+      class="pointer-events-none absolute top-0 right-0 transform translate-y-2.5 -translate-x-1/2"
+    />
+  </div>
+</template>
