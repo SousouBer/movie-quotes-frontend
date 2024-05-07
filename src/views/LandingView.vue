5e344e5c85375e8641cd6ea3e9c99f39881d314f
Soso <soso.beriashvili@redberry.ge>
2024-05-07 16:17:39 +0400

    style: add responsive styles to landing first page

--- src/views/LandingView.vue
+++ src/views/LandingView.vue
@@ -7,7 +7,7 @@ import ButtonBase from "@/components/ui/ButtonBase.vue";
   <TheHeader />
   <div class="bg-dark-shade-of-blue flex flex-col items-center py-32">
     <h1
-      class="text-5xl leading-normal font-black text-shade-of-beige h-5/6 text-center mb-4"
+      class="text-2xl sm:text-5xl leading-normal font-black text-shade-of-beige h-5/6 text-center mb-6 sm:mb-4"
     >
       Find any quotes in <br />millions of movie lines
     </h1>
