c7d9217f40cb26f66fbd94d37f4391e58f2a780a
Soso <soso.beriashvili@redberry.ge>
2024-05-07 16:16:08 +0400

    style: add markup for landing first page

--- src/views/LandingView.vue
+++ src/views/LandingView.vue
@@ -1,8 +1,16 @@
 <script setup lang="ts">
-import TheHeader from '@/components/shared/TheHeader.vue'
+import TheHeader from "@/components/shared/TheHeader.vue";
+import ButtonBase from "@/components/ui/ButtonBase.vue";
 </script>
 
 <template>
   <TheHeader />
-  <h1>Hello there!</h1>
+  <div class="bg-dark-shade-of-blue flex flex-col items-center py-32">
+    <h1
+      class="text-5xl leading-normal font-black text-shade-of-beige h-5/6 text-center mb-4"
+    >
+      Find any quotes in <br />millions of movie lines
+    </h1>
+    <ButtonBase label="Get started" />
+  </div>
 </template>
