08489682076e34c2bacf5dfddf0c32dd6b3adb67
Soso <soso.beriashvili@redberry.ge>
2024-05-07 19:43:11 +0400

    fix: fix lineheight of quotes in landing view

--- src/components/layouts/LayoutsLandingMovie.vue
+++ src/components/layouts/LayoutsLandingMovie.vue
@@ -17,14 +17,15 @@ const props = defineProps<{
       class="absolute top-0 right-0 min-h-screen w-full landing-images-blurr-overlay flex items-center pl-16 sm:pl-48"
     >
       <div class="relative flex flex-col gap-2 w-4/5 sm:w-3/5">
-        <span class="text-white text-2xl sm:text-4xl font-bold leading-normal"
+        <span
+          class="text-white text-2xl sm:text-4xl font-bold sm:leading-relaxed leading-relaxed"
           >"{{ props.movieQuote }}"</span
         >
         <span class="text-gray-300 font-bold text-lg sm:text-2xl"
           >{{ props.movieTitle }}
         </span>
         <IconQuoteLine
-          class="absolute top-5 sm:left-0 transform -translate-x-6 sm:-translate-x-16 -translate-y-1/2 w-4 sm:w-auto"
+          class="absolute top-5 sm:top-7 sm:left-0 transform -translate-x-6 sm:-translate-x-16 -translate-y-1/2 w-4 sm:w-auto"
         />
       </div>
     </div>
