38a2c2452ea66d2e3844d3f17e20d159e5faab15
Soso <soso.beriashvili@redberry.ge>
2024-05-12 15:16:44 +0400

    style: add register modal in root component

--- src/App.vue
+++ src/App.vue
@@ -1,5 +1,12 @@
-<script setup lang="ts"></script>
+<script setup lang="ts">
+import ModalRegister from "@/components/modals/ModalRegister.vue";
+</script>
 
 <template>
   <RouterView />
+  <Teleport to="body">
+    <ModalRegister
+      class="absolute top-1/2 left-1/2 transform -translate-y-1/2 -translate-x-1/2"
+    />
+  </Teleport>
 </template>
