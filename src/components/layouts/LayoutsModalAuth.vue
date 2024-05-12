8523aea1264843535354c95bb4b7a0676c175afd
Soso <soso.beriashvili@redberry.ge>
2024-05-12 23:05:21 +0400

    style: add mobile design to auth form modals

--- src/components/layouts/LayoutsModalAuth.vue
+++ src/components/layouts/LayoutsModalAuth.vue
@@ -5,7 +5,7 @@
     class="absolute top-0 left-0 bg-black bg-opacity-75 backdrop-blur-sm h-screen w-screen"
   >
     <div
-      class="bg-blueish-black flex flex-col justify-center rounded-xl px-28 py-12 w-2/5"
+      class="bg-blueish-black flex flex-col sm:justify-center rounded-xl px-7 sm:px-28 pt-28 sm:py-12 w-full h-full sm:h-auto sm:w-2/5"
     >
       <slot />
     </div>
