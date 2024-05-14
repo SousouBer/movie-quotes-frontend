62656c556735ef572ed13160fdcee43a3d5420fe
Soso <soso.beriashvili@redberry.ge>
2024-05-14 22:44:18 +0400

    feat: setup store for auth modals

--- src/App.vue
+++ src/App.vue
@@ -2,6 +2,30 @@
 import LayoutsModalAuth from "@/components/layouts/LayoutsModalAuth.vue";
 
 import ModalRegister from "@/components/modals/ModalRegister.vue";
+import ModalLogin from "@/components/modals/ModalLogin.vue";
+import ModalForgotPassword from "@/components/modals/ModalForgotPassword.vue";
+import ModalResetPassword from "@/components/modals/ModalResetPassword.vue";
+
+import ModalHttpResponse from "@/components/modals/ModalHttpResponse.vue";
+
+import { useAuthModalStore } from "@/stores/useAuthModalStore.ts";
+import { computed, type Component } from "vue";
+
+const store = useAuthModalStore();
+
+const authModal = computed<Component | null>(() => {
+  if (store.modalType === "register") {
+    return ModalRegister;
+  } else if (store.modalType === "login") {
+    return ModalLogin;
+  } else if (store.modalType === "forgotPassword") {
+    return ModalForgotPassword;
+  } else if (store.modalType === "resetPassword") {
+    return ModalResetPassword;
+  } else {
+    return null;
+  }
+});
 </script>
 
 <template>
@@ -11,7 +35,8 @@ import ModalRegister from "@/components/modals/ModalRegister.vue";
       class="absolute top-0 left-0 w-screen h-screen flex items-center justify-center"
     >
       <!-- Here will probably go auth modals! -->
-      <ModalRegister />
+      <!-- <ModalHttpResponse /> -->
+      <component :is="authModal" />
       />
     </LayoutsModalAuth>
   </Teleport>
---
+++ src/stores/useAuthModalStore.ts
@@ -0,0 +1,16 @@
+import { defineStore } from "pinia";
+import { ref, computed } from "vue";
+
+type State = "register" | "login" | "forgotPassword" | "resetPassword";
+
+export const useAuthModalStore = defineStore("authModal", () => {
+  const modalType: State = ref("register");
+
+  const getModalType = computed<State>(() => modalType.value);
+
+  function setModalType(changeModalType: State): void {
+    modalType.value = changeModalType;
+  }
+
+  return { modalType, getModalType, setModalType };
+});
