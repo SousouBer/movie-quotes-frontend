2404f5e80096461c6e4de103b0d8d6d96e07a131
Soso <soso.beriashvili@redberry.ge>
2024-05-15 22:25:41 +0400

    feat: add I18n internationalization plugin

--- src/main.ts
+++ src/main.ts
@@ -2,14 +2,18 @@ import "@/assets/main.css";
 
 import { createApp } from "vue";
 import { createPinia } from "pinia";
+import { createI18n } from "vue-i18n";
+
 import "@/plugins/vee-validate/rules";
 
 import App from "@/App.vue";
 import router from "@/router";
 
 const app = createApp(App);
+const i18n = createI18n();
 
 app.use(createPinia());
 app.use(router);
+app.use(i18n);
 
 app.mount("#app");
