68816a19c7c22a4e6c35d82b5c782079d339b11a
Soso <soso.beriashvili@redberry.ge>
2024-05-15 22:16:08 +0400

    refactor: use google redirection link from .env

--- src/components/base/BaseButtonGoogle.vue
+++ src/components/base/BaseButtonGoogle.vue
@@ -4,11 +4,13 @@ import IconGoogle from "@/components/icons/IconGoogle.vue";
 const props = defineProps<{
   label: string;
 }>();
+
+const googleAuthAPI = import.meta.env.VITE_API_GOOGLE_AUTH;
 </script>
 
 <template>
   <a
-    href="http://127.0.0.1:8000/auth/google/redirect"
+    :href="googleAuthAPI"
     class="text-white bg-transparent border border-white px-6 py-2 rounded-md whitespace-nowrap flex items-center justify-center gap-3 w-full mb-6"
   >
     <IconGoogle />
