507b67498c2983af37c13fed4518d39cc68fc006
Soso <soso.beriashvili@redberry.ge>
2024-05-17 21:38:22 +0400

    style: add main markup for aside

---
+++ src/components/shared/TheDashboard.vue
@@ -0,0 +1,28 @@
+<script setup lang="ts">
+import IconHouse from "@/components/icons/IconHouse.vue";
+import IconMovie from "@/components/icons/IconMovie.vue";
+</script>
+
+<template>
+  <aside class="bg-blueish-black px-16 py-8 w-[30%] h-screen">
+    <div class="flex items-center gap-5 cursor-pointer mb-10">
+      <div class="w-14 h-14 rounded-full border border-red-600">
+        <img class="bg-white" src="" alt="" />
+      </div>
+      <div class="flex flex-col justify-center">
+        <span class="text-2xl text-white">Nino Tabagari</span>
+        <span class="text-base text-gray-300">Edit your profile</span>
+      </div>
+    </div>
+    <div class="flex flex-col justify-center gap-10 pl-2">
+      <div class="text-white flex items-center gap-6 cursor-pointer">
+        <IconHouse />
+        <span class="pt-[0.5rem]">News Feed</span>
+      </div>
+      <div class="text-white flex items-center gap-6 cursor-pointer w-auto">
+        <IconMovie />
+        <span class="pt-[0.5rem]">List of Movies</span>
+      </div>
+    </div>
+  </aside>
+</template>
---
+++ src/views/ProfileView.vue
@@ -0,0 +1,11 @@
+<script setup lang="ts">
+import TheHeader from "@/components/shared/TheHeader.vue";
+import TheDashboard from "@/components/shared/TheDashboard.vue";
+</script>
+
+<template>
+  <div>
+    <TheHeader />
+    <TheDashboard />
+  </div>
+</template>
