879bc1382daeb9ec1bebd41ca49434677d0663c3
Soso <soso.beriashvili@redberry.ge>
2024-05-19 14:20:07 +0400

    refactor: remove csrf requests from where they are not needed

--- src/services/auth.ts
+++ src/services/auth.ts
@@ -1,12 +1,10 @@
 import Axios from "@/plugins/axios/axios";
 
 export async function login(payload) {
-  await Axios.get("/sanctum/csrf-cookie");
   return await Axios.post("/api/login", payload);
 }
 
 export async function register(payload) {
-  await Axios.get("/sanctum/csrf-cookie");
   return await Axios.post("/api/register", payload);
 }
 
@@ -21,17 +19,14 @@ export async function verifyEmail(url) {
 }
 
 export async function resendEmailVerificationLink(email) {
-  await Axios.get("/sanctum/csrf-cookie");
   return await Axios.post(`/api/resend-email-verification/${email}`);
 }
 
 export async function forgotPassword(payload) {
-  await Axios.get("/sanctum/csrf-cookie");
   return await Axios.post("/api/forgot-password", payload);
 }
 
 export async function resetPassword(payload, email, token) {
-  await Axios.get("/sanctum/csrf-cookie");
   return await Axios.post(`/api/reset-password/${email}/${token}`, payload);
 }
 
