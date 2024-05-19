4e503942231569908ea43acd35517c3de0501179
Soso <soso.beriashvili@redberry.ge>
2024-05-19 19:20:59 +0400

    wip: style profile confirm changes modal

--- src/components/profile/ModalProfileConfirmChanges.vue
+++ src/components/profile/ModalProfileConfirmChanges.vue
@@ -5,15 +5,21 @@ import BaseButton from "@/components/base/BaseButton.vue";
 <template>
   <div
     style="
-      background: linear-gradient(180deg, #000000 0%, rgba(0, 0, 0, 0.25) 100%);
+      background: linear-gradient(
+        112.94deg,
+        rgba(239, 239, 239, 0.3) -1.81%,
+        rgba(239, 239, 239, 0.00514528) 102.5%,
+        rgba(1, 1, 1, 0.00260417) 102.51%,
+        rgba(239, 239, 239, 0.05) 102.52%
+      );
     "
-    class="text-center pt-16 mx-10"
+    class="text-center pt-[4.5rem] mx-10 rounded-[10px]"
   >
     <span class="text-base text-white">Are you sure to make changes ?</span>
-    <div class="border-t border-gray-300 mt-14">
-      <div class="flex justify-between py-4 px-6">
+    <div class="border-t border-gray-300 mt-12">
+      <div class="flex justify-between py-4 px-4">
         <button class="text-base text-gray-300">Cancel</button>
-        <BaseButton label="Edit" />
+        <BaseButton class="!px-2" label="Confirm" />
       </div>
     </div>
   </div>
