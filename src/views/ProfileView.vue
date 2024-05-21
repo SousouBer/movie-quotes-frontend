d4cc8f117199053d7eb9b9fea5b8a625739166ae
Soso <soso.beriashvili@redberry.ge>
2024-05-21 23:09:46 +0400

    chore: remove long test avatar links from profile view

--- src/views/ProfileView.vue
+++ src/views/ProfileView.vue
@@ -39,12 +39,9 @@ type ValidationSchemaProfile = {
         >
           <FormProfilePictureField
             class="hidden sm:flex absolute top-0 left-1/3 transform translate-y-1/3 -translate-x-1/2"
-            avatar="https://images-wixmp-ed30a86b8c4ca887773594c2.wixmp.com/f/289e3a88-77b3-43d5-b8b0-7fce0c0cfa61/d83lria-7fb61469-45db-4325-9460-e5f0440cdf02.jpg/v1/fill/w_200,h_200,q_75,strp/the_legend_of_zelda__link_avatar_by_nightfall1007_d83lria-fullview.jpg?token=eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJzdWIiOiJ1cm46YXBwOjdlMGQxODg5ODIyNjQzNzNhNWYwZDQxNWVhMGQyNmUwIiwiaXNzIjoidXJuOmFwcDo3ZTBkMTg4OTgyMjY0MzczYTVmMGQ0MTVlYTBkMjZlMCIsIm9iaiI6W1t7ImhlaWdodCI6Ijw9MjAwIiwicGF0aCI6IlwvZlwvMjg5ZTNhODgtNzdiMy00M2Q1LWI4YjAtN2ZjZTBjMGNmYTYxXC9kODNscmlhLTdmYjYxNDY5LTQ1ZGItNDMyNS05NDYwLWU1ZjA0NDBjZGYwMi5qcGciLCJ3aWR0aCI6Ijw9MjAwIn1dXSwiYXVkIjpbInVybjpzZXJ2aWNlOmltYWdlLm9wZXJhdGlvbnMiXX0.sFAJF6HlcSjGXHP-jiAn_a_JCE79xwR8RPcCY3xqjLo"
+            avatar="test link"
           />
-          <FormProfilePictureField
-            class="sm:hidden mt-8"
-            avatar="https://images-wixmp-ed30a86b8c4ca887773594c2.wixmp.com/f/289e3a88-77b3-43d5-b8b0-7fce0c0cfa61/d83lria-7fb61469-45db-4325-9460-e5f0440cdf02.jpg/v1/fill/w_200,h_200,q_75,strp/the_legend_of_zelda__link_avatar_by_nightfall1007_d83lria-fullview.jpg?token=eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJzdWIiOiJ1cm46YXBwOjdlMGQxODg5ODIyNjQzNzNhNWYwZDQxNWVhMGQyNmUwIiwiaXNzIjoidXJuOmFwcDo3ZTBkMTg4OTgyMjY0MzczYTVmMGQ0MTVlYTBkMjZlMCIsIm9iaiI6W1t7ImhlaWdodCI6Ijw9MjAwIiwicGF0aCI6IlwvZlwvMjg5ZTNhODgtNzdiMy00M2Q1LWI4YjAtN2ZjZTBjMGNmYTYxXC9kODNscmlhLTdmYjYxNDY5LTQ1ZGItNDMyNS05NDYwLWU1ZjA0NDBjZGYwMi5qcGciLCJ3aWR0aCI6Ijw9MjAwIn1dXSwiYXVkIjpbInVybjpzZXJ2aWNlOmltYWdlLm9wZXJhdGlvbnMiXX0.sFAJF6HlcSjGXHP-jiAn_a_JCE79xwR8RPcCY3xqjLo"
-          />
+          <FormProfilePictureField class="sm:hidden mt-8" avatar="test link" />
           <FormProfile />
         </div>
         <FormProfileActionButtons
