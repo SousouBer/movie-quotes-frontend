5210dcf41087338a1d46abd47c34c7e009c57f09
Soso <soso.beriashvili@redberry.ge>
2024-05-25 02:48:23 +0400

    style: style success notification profile mobile version

---
+++ src/components/profile/FormProfileNotificationSuccess.vue
@@ -0,0 +1,16 @@
+<script setup lang="is">
+import IconSuccess from "@/components/icons/IconSuccess.vue";
+import IconCancel from "@/components/icons/IconCancel.vue";
+</script>
+
+<template>
+  <div class="rounded bg-green-200">
+    <div>
+      <IconSuccess />
+      <span class="text-base text-emerald-800"
+        >Changes updated succsessfully</span
+      >
+    </div>
+    <IconCancel />
+  </div>
+</template>
