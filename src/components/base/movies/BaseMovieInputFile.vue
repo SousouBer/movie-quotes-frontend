a323e24cabf967e900dee5876927ac60dff0f4cb
Soso <soso.beriashvili@redberry.ge>
2024-05-28 02:43:09 +0400

    style: add mobile styles for movie poster input

--- src/components/base/movies/BaseMovieInputFile.vue
+++ src/components/base/movies/BaseMovieInputFile.vue
@@ -2,6 +2,9 @@
 import { useField } from "vee-validate";
 
 import IconCamera from "@/components/icons/IconCamera.vue";
+import { computed } from "vue";
+import { ref } from "vue";
+import { onMounted } from "vue";
 
 const props = defineProps<{
   name: string;
@@ -10,6 +13,18 @@ const props = defineProps<{
 const { value, errorMessage, meta } = useField<string>(
   () => props.name as string,
 );
+
+const isMobileWidth = ref<boolean>(false);
+
+onMounted((): void => {
+  isMobileWidth.value = window.innerWidth < 700;
+});
+
+const dynamicHeading = computed((): string => {
+  return isMobileWidth.value
+    ? "Upload image"
+    : "Drag & drop your image here or";
+});
 </script>
 
 <template>
@@ -17,9 +32,9 @@ const { value, errorMessage, meta } = useField<string>(
     class="relative flex items-center gap-2 border border:shade-of-gray rounded py-4 px-3"
   >
     <IconCamera />
-    <span class="text-white text-xl">Drag & drop your image here or</span>
+    <span class="text-white text-xl">{{ dynamicHeading }}</span>
     <button
-      class="ml-2 bg-custom-purple transition-colors duration-300 hover:bg-purple-800 text-white rounded-[2px] p-2.5 text-lg"
+      class="ml-auto sm:ml-2 whitespace-nowrap bg-custom-purple transition-colors duration-300 hover:bg-purple-800 text-white rounded-[2px] p-2.5 text-lg"
     >
       Choose file
     </button>
