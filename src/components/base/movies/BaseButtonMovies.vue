d3f0ce871ebd3866e0983a906adbfcbcf1395058
Soso <soso.beriashvili@redberry.ge>
2024-05-27 18:45:20 +0400

    style(movies): style base button in movies' view

---
+++ src/components/base/movies/BaseButtonMovies.vue
@@ -0,0 +1,14 @@
+<script setup lang="ts">
+const props = defineProps<{
+  label: string;
+}>();
+</script>
+
+<template>
+  <button
+    class="flex items-center justify-center gap-2 bg-bright-red text-white rounded-[4.8px] py-[9px] px-[17px] transition-colors duration-200 hover:bg-red-500"
+  >
+    <slot />
+    {{ props.label }}
+  </button>
+</template>
---
+++ src/views/MoviesView.vue
@@ -0,0 +1,17 @@
+<script setup lang="ts">
+import TheHeader from "@/components/shared/TheHeader.vue";
+import BaseButtonMovies from "@/components/base/movies/BaseButtonMovies.vue";
+
+import IconMovieAdd from "@/components/icons/IconMovieAdd.vue";
+</script>
+
+<template>
+  <TheHeader />
+  <section class="bg-custom-gradient h-screen w-full">
+    <div>
+      <BaseButtonMovies label="Add Movie">
+        <IconMovieAdd />
+      </BaseButtonMovies>
+    </div>
+  </section>
+</template>
