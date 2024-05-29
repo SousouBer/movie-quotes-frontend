1bf0d4e12c1f8aef3e605efea69b245de0d2877f
Soso <soso.beriashvili@redberry.ge>
2024-05-29 20:52:53 +0400

    feat: add genres dropdown with dynamic display

--- src/components/base/movie/BaseMovieChipGenre.vue
+++ src/components/base/movie/BaseMovieChipGenre.vue
@@ -1,9 +1,14 @@
 <script setup lang="ts">
 import IconModalCancel from "@/components/icons/IconModalCancel.vue";
 
-const props = defineProps<{
+type Props = {
   name: string;
-}>();
+  showCancelButton?: boolean;
+};
+
+const props = withDefaults(defineProps<Props>(), {
+  showCancelButton: true,
+});
 </script>
 
 <template>
@@ -11,6 +16,11 @@ const props = defineProps<{
     class="bg-shade-of-gray transition-colors duration-300 hover:bg-gray-600 flex gap-3 items-center justify-center rounded-sm p-0.5 px-2"
   >
     <span class="text-white text-sm">{{ props.name }}</span>
-    <IconModalCancel class="cursor-pointer" width="6" height="6" />
+    <IconModalCancel
+      v-if="showCancelButton"
+      class="cursor-pointer"
+      width="6"
+      height="6"
+    />
   </div>
 </template>
--- src/components/base/movie/BaseMovieInputGenre.vue
+++ src/components/base/movie/BaseMovieInputGenre.vue
@@ -2,23 +2,50 @@
 import BaseMovieChipGenre from "@/components/base/movie/BaseMovieChipGenre.vue";
 
 import IconDropdownLocale from "@/components/icons/IconDropdownLocale.vue";
+import { ref } from "vue";
 
 const props = defineProps<{
   placeholder: string;
 }>();
+
+const genreModalIsShown = ref<boolean>(false);
+
+const toggleGenreModal = (): void => {
+  genreModalIsShown.value = !genreModalIsShown.value;
+};
 </script>
 
 <template>
   <div
-    class="flex items-center justify-between gap-3 border border:shade-of-gray py-2 px-4 rounded-[4.8px] h-12"
+    class="relative flex items-center justify-between gap-3 border border:shade-of-gray py-2 px-4 rounded-[4.8px] h-12"
   >
     <div class="overflow-hidden flex items-center gap-2 h-full">
-      <BaseMovieChipGenre name="Science" />
-      <BaseMovieChipGenre name="History" />
-      <BaseMovieChipGenre name="Adventure" />
-      <BaseMovieChipGenre name="Supernatural" />
+      <BaseMovieChipGenre :showCancelButton="true" name="Science" />
+      <BaseMovieChipGenre :showCancelButton="true" name="History" />
+      <BaseMovieChipGenre :showCancelButton="true" name="Adventure" />
+      <BaseMovieChipGenre :showCancelButton="true" name="Supernatural" />
       <BaseMovieChipGenre name="Supernatural" />
     </div>
-    <IconDropdownLocale color="white" />
+    <IconDropdownLocale
+      :class="{ 'rotate-180': genreModalIsShown }"
+      class="cursor-pointer transition-all duration-200"
+      color="white"
+      @click="toggleGenreModal"
+    />
+    <div
+      v-if="genreModalIsShown"
+      class="w-full flex-wrap absolute -bottom-1 left-0 transform translate-y-full bg-dark-shade-of-blue border border:shade-of-gray rounded-[4.8px] flex gap-2 p-4 z-10"
+    >
+      <BaseMovieChipGenre :showCancelButton="false" name="Science" />
+      <BaseMovieChipGenre :showCancelButton="false" name="Science" />
+      <BaseMovieChipGenre :showCancelButton="false" name="Science" />
+      <BaseMovieChipGenre :showCancelButton="false" name="Science" />
+      <BaseMovieChipGenre :showCancelButton="false" name="Science" />
+      <BaseMovieChipGenre :showCancelButton="false" name="Science" />
+      <BaseMovieChipGenre :showCancelButton="false" name="Science" />
+      <BaseMovieChipGenre :showCancelButton="false" name="Science" />
+      <BaseMovieChipGenre :showCancelButton="false" name="Science" />
+      <BaseMovieChipGenre :showCancelButton="false" name="Science" />
+    </div>
   </div>
 </template>
