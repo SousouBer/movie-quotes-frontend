a2662a9178d7abd984126f429e4f73c9edae3f33
Soso <soso.beriashvili@redberry.ge>
2024-05-30 02:21:39 +0400

    style: add mobile styles for quote component

--- src/components/movie/MovieItemQuote.vue
+++ src/components/movie/MovieItemQuote.vue
@@ -3,17 +3,24 @@ import IconThreeDots from "@/components/icons/IconThreeDots.vue";
 </script>
 
 <template>
-  <div class="bg-dark-shade-of-blue rounded-[10px] w-[60%] p-8">
+  <div class="bg-dark-shade-of-blue rounded-[10px] sm:w-[60%] p-8">
     <div
-      class="relative flex gap-10 items-center border-b border-[#EFEFEF33] pb-8"
+      class="relative flex flex-col sm:flex-row gap-10 items-center border-b border-[#EFEFEF33] pb-8"
     >
       <div>
-        <img src="/images/dummy-quote-img.svg" alt="Quote picture" />
+        <img
+          class="h-full w-full"
+          src="/images/dummy-quote-img.svg"
+          alt="Quote picture"
+        />
       </div>
       <span class="text-2xl text-gray-300"
         >"Frankly, my dear, I don'tgive a damn."
       </span>
-      <IconThreeDots class="absolute top-0 right-0 cursor-pointer" />
+      <IconThreeDots
+        class="absolute -bottom-8 sm:top-0 right-0 cursor-pointer"
+      />
     </div>
+    <!-- Here will go comments and likes icons -->
   </div>
 </template>
