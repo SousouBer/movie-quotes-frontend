435bbdb641925cb7580c289f7439dce10630d7a3
Soso <soso.beriashvili@redberry.ge>
2024-06-02 16:43:05 +0400

    refactor: separate main error layout and add not found view

---
+++ src/components/layouts/LayoutsErrorView.vue
@@ -0,0 +1,41 @@
+<script setup lang="ts">
+import BaseButton from "@/components/base/BaseButton.vue";
+
+import { useRouter } from "vue-router";
+
+const router = useRouter();
+
+const props = defineProps<{
+  imageUrl: string;
+  imageAlt: string;
+  heading: string;
+  description: string;
+}>();
+
+const returnHome = (): void => {
+  router.push({ name: "landing" });
+};
+</script>
+
+<template>
+  <div
+    class="flex items-center justify-center bg-dark-shade-of-blue min-h-screen px-8 text-center"
+  >
+    <div class="flex items-center gap-3 sm:gap-8 flex-col">
+      <div class="relative z-50">
+        <img class="z-50" :src="props.imageUrl" :alt="props.imageAlt" />
+      </div>
+      <span class="text-white font-bold text-2xl sm:text-5xl">{{
+        props.heading
+      }}</span>
+      <span class="text-white font-medium text-base sm:text-2xl">{{
+        props.description
+      }}</span>
+      <BaseButton
+        @click="returnHome"
+        class="text-base sm:text-xl mt-6"
+        label="Return Home"
+      />
+    </div>
+  </div>
+</template>
--- src/views/ForbiddenView.vue
+++ src/views/ForbiddenView.vue
@@ -1,38 +1,12 @@
 <script setup lang="ts">
-import BaseButton from "@/components/base/BaseButton.vue";
-
-import { useRouter } from "vue-router";
-
-const router = useRouter();
-
-const returnHome = (): void => {
-  router.push({ name: "landing" });
-};
+import LayoutsErrorView from "@/components/layouts/LayoutsErrorView.vue";
 </script>
 
 <template>
-  <div
-    class="flex items-center justify-center bg-dark-shade-of-blue min-h-screen px-8 text-center"
-  >
-    <div class="flex items-center gap-3 sm:gap-8 flex-col">
-      <div class="relative z-50">
-        <img
-          class="z-50"
-          src="/images/Gandolf-forbidden-image.svg"
-          alt="Gandolf image"
-        />
-      </div>
-      <span class="text-white font-bold text-2xl sm:text-5xl"
-        >You shall not pass!</span
-      >
-      <span class="text-white font-medium text-base sm:text-2xl"
-        >Sorry, but you don’t have permission to access this page</span
-      >
-      <BaseButton
-        @click="returnHome"
-        class="text-base sm:text-xl mt-6"
-        label="Return Home"
-      />
-    </div>
-  </div>
+  <LayoutsErrorView
+    imageUrl="images/Gandolf-forbidden-image.svg"
+    imageAlt="Forbidden view image"
+    heading="You shall not pass!"
+    description="Sorry, but you don’t have permission to access this page"
+  />
 </template>
