b0d952b570e67d98a2fe27bb466f41aa36b406ad
Soso <soso.beriashvili@redberry.ge>
2024-06-11 21:51:25 +0400

    feat: fetch quote again when comment gets added

--- src/components/base/newsFeed/BaseNewsFeedCommentAdd.vue
+++ src/components/base/newsFeed/BaseNewsFeedCommentAdd.vue
@@ -5,58 +5,56 @@ import { Form as CommentForm, useField } from "vee-validate";
 import { addComment } from "@/services/quote";
 import type { CommentPayload } from "@/plugins/typescript/types";
 import axios from "axios";
-import { ref } from "vue";
 
-const { value } = useField<string>(() => "comment");
+const { value } = useField<string>("comment");
+
+import { useQuoteStore } from "@/stores/quote";
 
 const props = defineProps<{
   quote_id: number;
 }>();
 
-const submitCommentButton = ref<HTMLButtonElement | null>(null);
+const qouteStore = useQuoteStore();
 
-const handleSubmit = async (
-  values: any,
-  {
-    resetForm,
-  }: {
-    resetForm: () => void;
-  },
-) => {
+const handleSubmit = async (): Promise<void> => {
   try {
     const commentData: CommentPayload = {
       quote_id: props.quote_id,
       comment: value.value,
     };
 
-    await addComment(commentData);
+    const response = await addComment(commentData);
 
-    resetForm();
+    const updatedQuote = response.data.data;
+
+    const quoteIndex = qouteStore.quotes?.findIndex(
+      (quote) => quote.id === props.quote_id,
+    );
+
+    if (quoteIndex !== -1) {
+      // Replace the existing quote with the updated one
+      qouteStore.quotes?.splice(quoteIndex as number, 1, updatedQuote);
+    }
+
+    value.value = "";
   } catch (error: any) {
     if (axios.isAxiosError(error)) {
       console.log("An error occured: ", error);
     }
   }
 };
-
-const triggerSubmit = (event: KeyboardEvent) => {
-  event.preventDefault();
-  submitCommentButton.value?.click();
-};
 </script>
 
 <template>
   <CommentForm @submit="handleSubmit" class="flex items-center gap-4">
     <BaseUserDetails :isNewsFeedUser="true" :showUsername="false" />
-    {{ value }}
     <input
       name="comment"
       class="bg-grayish-purple w-full outline-none text-base sm:text-xl text-white rounded-[10px] px-4 sm:px-8 py-2 sm:py-3"
       type="text"
       placeholder="Write a comment"
       v-model="value"
-      @keyup.enter="triggerSubmit"
     />
-    <button ref="submitCommentButton" class="hidden">Submit Form</button>
+    <button class="hidden">Submit Form</button>
   </CommentForm>
 </template>
